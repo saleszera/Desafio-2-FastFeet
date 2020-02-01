53c34d50ba335e0e815d42ea1a60d5591069a881
saleszera <crowzcoleslaw@gmail.com>
2020-02-01 20:19:24 -0300

    Criada a função put que só pode ser feita se o usuario estiver logado

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -33,6 +33,52 @@ class UserController {
       provider,
     });
   }
+
+  async put(req, res) {
+    const schema = Yup.object().shape({
+      nome: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string()
+        .min(6)
+        .when('password', (password, field) =>
+          password ? field.required().onOf([Yup.ref('password')]) : field
+        ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ Error: 'Validation fails' });
+    }
+
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ Error: 'User already exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ Error: 'Password does not match' });
+    }
+
+    const { id, nome } = await user.update(req.body);
+
+    return res.json({
+      id,
+      nome,
+      email,
+    });
+  }
 }
 
 export default new UserController();
