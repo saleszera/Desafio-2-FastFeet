59743f853bfc8d3c9a6b9b4abcfcc6b668e7cf03
saleszera <crowzcoleslaw@gmail.com>
2020-02-01 09:04:08 -0300

    configurando sequelize

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+
+module.exports = {
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  define: {
+    // timestamps garante que haverá uma coluna updated_at e created_at dentro de cada tabela do banco de dados
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
