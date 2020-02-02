166c83a2a7d411344642ca55d691f4ec0e67ed9d
saleszera <crowzcoleslaw@gmail.com>
2020-02-02 00:39:55 -0300

    Adicionada a rota para criação de destinatários assim como inserido o middleware que permite com que apenas o admin insira destinatarios

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
-
+// controllers
 import UserController from './app/controllers/UserController';
 import SessionControler from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
+// middlewares
 import authMiddleware from './app/middlewares/auth';
+import onlyAdminMiddleware from './app/middlewares/onlyAdmin';
 
 const routes = new Router();
 
@@ -11,5 +14,7 @@ routes.post('/sessions', SessionControler.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.put);
+routes.use(onlyAdminMiddleware);
+routes.post('/recipient', RecipientController.store);
 
 export default routes;
