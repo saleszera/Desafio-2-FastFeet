a73da32032fc804a2ed940805e2148b4e4a96d5b
saleszera <crowzcoleslaw@gmail.com>
2020-02-02 00:34:46 -0300

    Adicionada a função que recebe o cep e retorna o endereço

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,4 @@
-import 'dotenv/config';
+require('dotenv/config');
 
 module.exports = {
   dialect: process.env.DB_DIALECT,
@@ -6,6 +6,7 @@ module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
   define: {
     // timestamps garante que haverá uma coluna updated_at e created_at dentro de cada tabela do banco de dados
     timestamps: true,
---
+++ src/utils/searchCep.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+async function searchCep(data) {
+  const endereco = await axios.get(`https://viacep.com.br/ws/${data}/json/`);
+  return endereco.data;
+}
+
+export default searchCep;
