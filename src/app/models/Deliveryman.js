56512f6d8473275b1dfc132a841a141105016115
saleszera <crowzcoleslaw@gmail.com>
2020-02-10 21:42:47 -0300

    criada função estatica para associação da tabela deliverymans com files

--- src/app/models/Deliveryman.js
+++ src/app/models/Deliveryman.js
@@ -4,12 +4,16 @@ class Deliveryman extends Model {
   static init(sequelize) {
     super.init(
       {
-        nome: Sequelize.STRING,
+        name: Sequelize.STRING,
         email: Sequelize.STRING,
       },
       { sequelize }
     );
     return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
   }
 }
 
