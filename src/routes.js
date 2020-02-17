84364fa0cbcc57eb0d3d9467ab17d83eebd2907a
saleszera <crowzcoleslaw@gmail.com>
2020-02-17 19:32:47 -0300

    alterados parametros de algumas rotas para facilitar o entendimento

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,8 @@ import FileController from './app/controllers/FileController';
 import OrderController from './app/controllers/OrderController';
 import DeliveryAccessController from './app/controllers/DeliveryAccessController';
 import DeliveryProblemController from './app/controllers/DeliveryProblemController';
+import DeliveryStart from './app/controllers/DeliveryStart';
+import DeliveryEnd from './app/controllers/DeliveryEnd';
 // middlewares
 import authMiddleware from './app/middlewares/auth';
 import onlyAdminMiddleware from './app/middlewares/onlyAdmin';
@@ -21,20 +23,26 @@ routes.post('/users', UserController.store);
 
 routes.post('/sessions', SessionControler.store);
 
-routes.post('/delivery/:id/problems', DeliveryProblemController.store);
+routes.post('/delivery/:orderId/problems', DeliveryProblemController.store);
 
 routes.get('/deliveryman/:id', DeliveryAccessController.index);
-routes.put('/deliveryman/:id', DeliveryAccessController.withdraw);
-routes.post(
-  '/deliveryman/:deliverymanId/:order',
-  upload.single('file'),
-  DeliveryAccessController.signature
+routes.put(
+  '/deliveryman/:deliverymanId/start/order/:orderId',
+  DeliveryStart.store
 );
-routes.get('/deliveryman/:id/deliveries', DeliveryAccessController.deliveries);
+routes.put(
+  '/deliveryman/:deliverymanId/end/order/:orderId',
+  upload.single('file'),
+  DeliveryEnd.put
+);
+routes.get(
+  '/deliveryman/:deliverymanId/deliveries',
+  DeliveryAccessController.deliveries
+);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.put);
-routes.delete('/users/:id', UserController.delete);
+routes.delete('/users/:userId', UserController.delete);
 
 routes.use(onlyAdminMiddleware);
 routes.get('/users', UserController.index);
@@ -43,23 +51,27 @@ routes.post('/recipient', RecipientController.store);
 routes.get('/recipient', RecipientController.index);
 routes.put('/recipient', RecipientController.put);
 
-routes.delete('/recipient/:id', RecipientController.delete);
+routes.delete('/recipient/:recipientId', RecipientController.delete);
 
 routes.post('/deliverymans', DeliverymanController.store);
 routes.get('/deliverymans', DeliverymanController.index);
-routes.delete('/deliverymans/:id', DeliverymanController.delete);
-routes.put('/deliverymans/:id', DeliverymanController.put);
-routes.get('/deliverymans/:id', DeliverymanController.show);
+routes.delete('/deliverymans/:deliverymanId', DeliverymanController.delete);
+routes.put('/deliverymans/:deliverymanId', DeliverymanController.put);
+routes.get('/deliverymans/:deliverymanId', DeliverymanController.show);
 
-routes.post('/files', upload.single('file'), FileController.store);
+routes.post(
+  '/avatar/deliveryman/:deliverymanId',
+  upload.single('file'),
+  FileController.store
+);
 
 routes.post('/orders', OrderController.store);
 routes.get('/orders', OrderController.index);
-routes.get('/orders/:id', OrderController.show);
+routes.get('/orders/:orderId', OrderController.show);
 routes.delete('/orders/:id', OrderController.delete);
 routes.put('/orders/:id', OrderController.put);
 
 routes.get('/delivery/problems', DeliveryProblemController.index);
-routes.get('/delivery/:id/problems', DeliveryProblemController.show);
+routes.get('/delivery/:deliverymanId/problems', DeliveryProblemController.show);
 
 export default routes;
