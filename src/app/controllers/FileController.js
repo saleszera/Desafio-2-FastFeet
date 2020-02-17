c723042d60f46e0d7366aaa6251e4e6fa4190689
saleszera <crowzcoleslaw@gmail.com>
2020-02-17 19:35:13 -0300

    agora quando é inserida uma imagem a mesma já é atribuida ao id de um entregador que é indormado no parametro da requisição

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -1,12 +1,31 @@
 import File from '../models/File';
+import Deliveryman from '../models/Deliveryman';
 
 class FileController {
   async store(req, res) {
+    const deliveryman = await Deliveryman.findOne({
+      where: { id: req.params.deliverymanId },
+      attributes: ['id', 'name', 'email', 'avatar_id'],
+      include: {
+        model: File,
+        as: 'avatar',
+        attributes: ['name', 'path', 'url'],
+      },
+    });
+
+    if (!deliveryman) {
+      return res.status(400).json({ Error: 'Deliveryman Does not exist!' });
+    }
+
     const { originalname: name, filename: path } = req.file;
 
     const file = await File.create({ name, path });
 
-    return res.json(file);
+    const insertAvatar = await deliveryman.update({
+      avatar_id: file.id,
+    });
+
+    return res.json(insertAvatar);
   }
 }
 
